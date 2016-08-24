47e4a11b68bf84289234fdf7445d7d02feee7870
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-24 16:10:05 -0500

    fix: do not require entity name

--- blueprints/ember-filesystem/index.js
+++ blueprints/ember-filesystem/index.js
@@ -1,6 +1,7 @@
 /*jshint node:true*/
 module.exports = {
   description: '',
+  normalizeEntityName: function() {},
 
   // locals: function(options) {
   //   // Return custom template variables here.
